948e097397e5a4b7f9ecaf9ebf32993bd537e29a
kurmanzhan Muhtarova <kurmanzhanmuhtarova@gmail.com>
2022-04-10 13:46:29 -1000

    created navbar and home page

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+function Home() {
+  return (
+    <div name="home" className="w-full h-screen bg-[#222020] color-wite">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi,my name is</p>
+        <h1 className=" text-4xl font-bold text-[#ccc] sm:text-6xl ">
+          Kurmanzhan Mukhtarova
+        </h1>
+        <h2 className=" text-4xl font-bold text-[#756c6c] sm:text-6xl ">
+          I'm frontend developer
+        </h2>
+        <p className="text-[#605555] py-5 sm:text-2xl max-w-[700px] ">
+          creative frontend devoloper self motivated team player // should add
+          text
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-6 flex items-center hover:bg-pink-600">
+            View works
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-5 " />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
